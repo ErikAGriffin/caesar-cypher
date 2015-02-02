69d137651126217e2ca9518a5f692ca953235f80
Erik Griffin <griffin.erik@gmail.com>
2015-02-02 21:55:08 +0000

    2 lines, but does not support spaces

--- cypher.rb
+++ cypher.rb
@@ -1,9 +1,12 @@
+# Caesar cypher challenge.
+# Point is to create shortest encoder / decoder.
+# Currently does not support spaces or special characters
+
 def encode(message)
-  message = message.downcase
   char_array = [*'a'..'z'].rotate(-1)
-  message.chars.map! { |x| x = char_array[(char_array.index(x)+3)%26]}.join
+  message.downcase!.chars.map! { |x| x = char_array[(char_array.index(x)+3)%26]}.join
 end
 
 
-puts encode('abcdewxyz')
+puts encode('FOXY')
 
