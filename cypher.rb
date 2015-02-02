07c79cf0eb18c3ea38215be8c831a40137717132
Erik Griffin <griffin.erik@gmail.com>
2015-02-02 22:23:57 +0000

    Added single encoder/decoder method, takes (String,BOOLEAN)

--- cypher.rb
+++ cypher.rb
@@ -2,8 +2,8 @@
 # Point is to create shortest encoder / decoder.
 # 2 lines, although second isn't very clear at all.
 # The rotate is unnecessary but helps human understanding of A = 1, B = 2 etc.
+# Also available  is singe encoder/decoder that takes a string and a boolean indicating ifEncoding
 # # # # # # # # # # # # # # # # # # # #
-# Ughh, downcase! returns nil if the string is already downcased.. why would it do that??
 
 
 def encode(message)
@@ -16,6 +16,11 @@ def decode(message)
   message.downcase.chars.map! { |x| char_array.include?(x) ? x = char_array[(char_array.index(x)-3)%26] : x }.join
 end
 
+def coder(message,isEncoding)
+  isEncoding ? z = 3 : z = -3
+  char_array = [*'a'..'z'].rotate(-1)
+  message.downcase.chars.map! { |x| char_array.include?(x) ? x = char_array[(char_array.index(x)+z)%26] : x }.join
+end
 
 
 
@@ -23,3 +28,6 @@ myString = encode('ANY phrase AT*ALL!')
 
 puts myString
 puts decode(myString)
+
+puts myString = coder('My message',true)
+puts coder(myString,false)
